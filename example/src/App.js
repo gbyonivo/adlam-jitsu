8cb98d550823ef75fc672544263b39db7cd8fd76
gbyon <gbyonivo@gmail.com>
2020-05-11 11:22:56 +0100

    the beginning

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'adlam-jitsu'
+import 'adlam-jitsu/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
